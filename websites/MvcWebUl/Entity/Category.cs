6fa99f8da4a9ec1b293d1468e09d4be05cd9ce98
BatuhanBaşay <batuhan.basay@hotmail.com>
2022-03-24 17:59:56 +0300

    update

--- websites/MvcWebUl/Entity/Category.cs
+++ websites/MvcWebUl/Entity/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace MvcWebUl.Entity
     public class Category
     {
         public int Id { get; set; }
+
+        [StringLength(maximumLength: 20, ErrorMessage = "En fazla 20 karakter girebilirsiniz")]
         public string Name { get; set; }
         public string Description { get; set; }
 
